0535be74112e1d2f7fdfe6467cb966a926d9661e
apedley <apedley@gmail.com>
2015-10-21 21:09:40 -0500

    Basic reqs finished

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,30 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
+    this.on('add', this.songAdded, this);
+    this.on('ended', this.songEnded, this);
+    this.on('dequeue', this.songRemoved, this);
+  },
+  songRemoved: function(song) {
+    this.remove(song);
+  },
+
+  songAdded: function() {
+    if (this.length === 1) {
+      this.playFirst();
+    }
+  },
+
+  playFirst: function() {
+    var song = this.first();
+    song.play();
+  },
+
+  songEnded: function() {
+    this.shift();
+    if (this.length > 0) {
+      this.playFirst();
+    }
   }
 
 });
